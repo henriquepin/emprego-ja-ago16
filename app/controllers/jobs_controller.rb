3ab4dcb4759ae7a2a4d0d5cf2d39603ace74803b
henrique-jailson <pin.henrique03@gmail.com>
2016-08-30 21:03:39 -0300

    usuario cadastra vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,18 @@
 class JobsController < ApplicationController
   def show
-    @job = Job.find params[:id] 
+    @job = Job.find params[:id]
+  end
+  def new
+    @job = Job.new
+  end
+  def create
+    #binding.pry
+    @job = Job.create job_params
+    redirect_to @job
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :location, :category, :company, :description)
   end
 end
