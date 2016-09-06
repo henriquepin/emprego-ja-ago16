8f917ab0fba062e304924c4737e5cffedadade4c
henrique-jailson <pin.henrique03@gmail.com>
2016-09-05 21:59:24 -0300

    usuario cadastra categoria sem sucesso WiP

--- spec/features/user_create_category_spec.rb
+++ spec/features/user_create_category_spec.rb
@@ -12,4 +12,16 @@ feature 'User create categories' do
 
     expect(page).to have_content(category.name)
   end
+
+  scenario 'unsucessfully' do
+    category = Category.new(name: 'Estagiario')
+
+    visit new_category_path
+
+    fill_in 'Nome',          with: ''
+
+    click_on 'Criar categoria'
+
+      
+  end
 end
